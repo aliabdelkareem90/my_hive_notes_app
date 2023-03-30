272cca04ead71a991f81b973019ba4022b4cb8e3
Ali Abdulkareem Shamit <aliabdelkareem90@gmail.com>
2023-03-30 22:55:28 +0300

    init hive and open notes box

--- lib/constants/constents.dart
+++ lib/constants/constents.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const aPrimaryColor = Colors.cyan;
+
+const String aNotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:hive_notes_app/views/notes_view.dart';
 
 import 'constants/constents.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(aNotesBox);
   runApp(const NotesApp());
 }
 
